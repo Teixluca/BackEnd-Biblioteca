ec3ff988f66da88c9f7e72ff7d08f219095f341f
Teixluca <lucas.alvesteixeira@outlook.com>
2024-12-10 14:20:57 -0300

    API de usuario não funcionando

---
+++ Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using ApiBiblioteca.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBiblioteca.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+          
+            private static List<UserModel> usuarios = new List<UserModel>();
+
+        [HttpPost]
+        public ActionResult<List<UserModel>> AdicionaUsuario(UserModel novo)
+        {
+   
+
+            novo.IdUser = usuarios.Count < 0 ? usuarios[usuarios.Count - 1].IdUser + 1 : 1;
+
+            usuarios.Add(novo);
+
+            return Ok(usuarios);
+
+
+        }
+
+        [HttpGet]
+        public ActionResult<List<UserModel>> BuscaUsuario()
+        {
+            return Ok(usuarios);
+        }
+
+
+
+    }
+}
---
+++ Model/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBiblioteca.Model
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserModel
+    {
+        public int IdUser { get; set; }
+        public int Id { get; set; }
+        public string NameUser { get; set; } = string.Empty;
+        public string EmailUser{ get; set; } = string.Empty;
+        public string AnoNasc { get; set; } = string.Empty;
+    }
+}
